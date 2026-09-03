05ba1651774ec94230dc93900b5dd3e49989c707
j-goodman <johncurtisgoodman@gmail.com>
2026-09-03 19:00:31 -0400

    Add dependecy arrays.

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
     fetch(`https://j-goodman.github.io/language-class-data/data/class-list.json`)
     .then(response => response.json())
     .then(data => setCourses(data))
-  })
+  }, [])
 
   useEffect(() => {
     fetch(`https://j-goodman.github.io/language-class-data/data/languages.json`)
     .then(response => response.json())
     .then(data => setLanguages(data.languages))
-  })
+  }, [])
 
   const course = courses.find(c => c.id === selectedId)
   const language = languages.find(lang => lang.language === course?.language)
