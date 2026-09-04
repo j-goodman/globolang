f41997f1022def7dd06aa32e41bf699619316c44
j-goodman <johncurtisgoodman@gmail.com>
2026-09-03 20:11:27 -0400

    Add the CourseList component with styling.

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,8 +1,14 @@
+import './CourseList.css'
+
 function CourseList({courses, languages}) {
     return (
-        <ul>
+        <ul className="course-list">
           {courses.map(course => {
-            return <li>{course.level} {course.language}</li>
+            const language = languages.find(l => l.language === course.language)
+            return <li className="course-list__item" key={`${course.level}-${course.language}`}>
+              <img className="course-list__flag" src={language?.['flag-icon']} alt={`${course.language} flag`} />
+              {course.level} {course.language}
+            </li>
           })}
         </ul>
     )
